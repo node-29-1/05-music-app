12edaeea00bec2dc2e48b3f379216a5c3979bccb
Andres Mendoza <andres.david.mm@hotmail.com>
2023-11-03 22:52:29 -0500

    added albums and songs

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,23 @@
+const Album = require('./Album');
 const Artist = require('./Artist');
 const Genre = require('./Genre');
+const Song = require('./Song');
 
 Artist.belongsToMany(Genre, { through: "ArtistsGenres" });
 Genre.belongsToMany(Artist, { through: "ArtistsGenres" });
 
+
+Artist.hasMany(Album);
+Album.belongsTo(Artist);
+
+
+Album.hasMany(Song);
+Song.belongsTo(Album);
+
+
+Song.belongsToMany(Artist, { through: "SongsArtists" });
+Artist.belongsToMany(Song, { through: "SongsArtists" });
+
+
+Song.belongsToMany(Genre, { through: "SongsGenres" });
+Genre.belongsToMany(Song, { through: "SongsGenres" });
